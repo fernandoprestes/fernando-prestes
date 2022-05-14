2a145360dfa8d4e4783fb184a977190190719d5b
fernandoprestes <fernandoprestesc@gmail.com>
2022-05-13 21:02:23 -0300

    fix: clear setup

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,3 @@
 <template>
-  <div class="container__background">
-    <router-view />
-  </div>
+  <router-view />
 </template>
-<style
-  lang="css"
-  scoped
->
-  .container__background {
-    background-image: linear-gradient(to right bottom, #2c0b61, #5d106b, #861c72, #ad2e76, #d04579);
-  }
-</style>
