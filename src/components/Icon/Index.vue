d7cb252cd50b369206686be83472b69da5655664
fernandoprestes <fernandoprestesc@gmail.com>
2022-05-21 22:34:16 -0300

    refactor: icons and rotur

--- src/components/Icon/Index.vue
+++ src/components/Icon/Index.vue
@@ -5,14 +5,19 @@
   />
 </template>
 <script>
+  import Folder from './Folder.vue';
   import Github from './Github.vue';
+  import Linkedin from './Linkedin.vue';
 
   export default {
-    components: { Github },
+    components: { Github, Folder, Linkedin },
     props: {
       name: {
         type: String,
         require: true,
+      },
+      color: {
+        type: String,
       },
     },
   };
--- src/router/index.js
+++ src/router/index.js
@@ -1,18 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router';
 
 const Home = () => import('../pages/Home/index.vue');
-const About = () => import('../pages/About/index.vue');
 
 export const routes = [
   {
     path: '/',
     name: 'Home',
     component: Home,
-  },
-  {
-    path: '/about',
-    name: 'About',
-    component: About,
   },
   {
     path: '/:pathMatch(.*)*',
