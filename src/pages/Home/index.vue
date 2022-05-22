54a3666a3acc655e6140731db0f50ccf8d1e81e3
fernandoprestes <fernandoprestesc@gmail.com>
2022-05-22 15:38:09 -0300

    feat: add links curses and tags tech

--- src/pages/Home/index.vue
+++ src/pages/Home/index.vue
@@ -14,30 +14,33 @@
       <div class="flex flex-col gap-5">
         <v-card class="flex flex-col items-center gap-4 text-center">
           <img
-            class="w-16 rounded-full border-2 border-bg-cards ring-2 ring-brand-color"
-            src="https://images.generated.photos/PT6UvTaFvGj79xdkE4n1HVArXgrjHCmZ0ma9rSRxLho/rs:fit:512:512/wm:0.95:sowe:18:18:0.33/czM6Ly9pY29uczgu/Z3Bob3Rvcy1wcm9k/LnBob3Rvcy92M18w/NTE3NDA5LmpwZw.jpg"
+            class="w-16 rounded-full border-2 border-bg-cards ring-2 ring-tech-vue"
+            src="https://github.com/fernandoprestes.png"
           />
           <div class="text-brand-color">
-            <h2 class="text-xl font-bold">Nome do infeliz</h2>
+            <h2 class="text-xl font-bold">Fernando Prestes</h2>
             <p class="text-lg">Desenvolvedor frontend</p>
           </div>
         </v-card>
-        <v-card class="h-full">
+        <v-card class="h-full space-y-4">
           <div class="flex items-center gap-2">
             <icon name="Github" />
-            git
+            <a
+              href="https://github.com/fernandoprestes"
+              target="_blank"
+              class="text-brand-color hover:brightness-150"
+              >/fernandoprestes</a
+            >
           </div>
           <div class="flex items-center gap-2">
             <icon name="Linkedin" />
-            git
+            <a
+              href="https://www.linkedin.com/in/fernandoprestes/"
+              target="_blank"
+              class="text-brand-color hover:brightness-150"
+              >/fernandoprestes</a
+            >
           </div>
-
-          <p>Lorem ipsum dolor sit</p>
-          <p>Lorem ipsum dolor sit</p>
-          <p>Lorem ipsum dolor sit</p>
-          <p>Lorem ipsum dolor sit</p>
-          <p>Lorem ipsum dolor sit</p>
-          <p>Lorem ipsum dolor sit</p>
         </v-card>
       </div>
       <div class="flex flex-1 flex-col gap-4">
@@ -93,6 +96,16 @@
         >
           JavaScript
         </div>
+        <div
+          class="w-fit rounded-3xl border border-bg-cards bg-tech-html px-4 text-sm ring-1 ring-tech-html hover:brightness-110"
+        >
+          HTML
+        </div>
+        <div
+          class="w-fit rounded-3xl border border-bg-cards bg-tech-css px-4 text-sm ring-1 ring-tech-css hover:brightness-110"
+        >
+          CSS
+        </div>
       </v-card>
     </div>
   </div>
@@ -102,18 +115,52 @@
         <h2 class="font-bold text-brand-color">Experiência</h2>
         <div class="flex w-full gap-4 rounded bg-brand-color-100 p-2 text-brand-color">
           <img
-            class="w-16"
-            src="https://thumbs.dreamstime.com/b/letter-k-drop-water-logo-vector-letter-k-drop-water-logo-vector-modern-logo-gear-shape-shape-suitable-entertainment-107014401.jpg"
+            class="w-16 rounded bg-white px-2"
+            src="./../../assets/img/logos/tegra.svg"
             alt=""
           />
           <div>
-            <h2 class="text-lg font-bold">Lorem</h2>
-            <p>lorem lorem</p>
+            <h2 class="text-lg font-bold">Tegra</h2>
+            <p class="text-sm font-bold italic">Outubro/2021 - até o momento</p>
+            <p>Desenvolvedor Frontend</p>
           </div>
         </div>
       </v-card>
-      <v-card>
+      <v-card class="space-y-2">
         <h2 class="font-bold text-brand-color">Cursos</h2>
+        <div class="flex w-full gap-4 rounded bg-brand-color-100 p-2 text-brand-color">
+          <img
+            class="w-16 px-2"
+            src="./../../assets/img/logos/alura.svg"
+            alt="Logo da escola Alura cursos online de Tecnologia"
+          />
+          <div>
+            <h2 class="text-lg font-bold">
+              <a href="https://www.alura.com.br/">Alura</a>
+            </h2>
+            <ul>
+              <li>Formação: Vue.js</li>
+              <li>Formação: Vue.js 3</li>
+              <li>Formação: React JS</li>
+            </ul>
+          </div>
+        </div>
+        <div class="flex w-full gap-4 rounded bg-brand-color-100 p-2 text-brand-color">
+          <img
+            class="w-16 px-2"
+            src="./../../assets/img/logos/origamid.svg"
+            alt="Logo da escola Origamid cursos de Web Design, UX/UI Design e Front End"
+          />
+          <div>
+            <h2 class="text-lg font-bold">
+              <a href="https://www.origamid.com">Origamid</a>
+            </h2>
+            <ul>
+              <li>Web Design Completo</li>
+              <li>JavaScript Completo ES6</li>
+            </ul>
+          </div>
+        </div>
       </v-card>
     </div>
   </div>
