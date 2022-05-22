3dd396e5f883a4bd853a72b9b16640c82444970d
fernandoprestes <fernandoprestesc@gmail.com>
2022-05-21 22:33:54 -0300

    feat: add config

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,10 @@ module.exports = {
       colors: {
         'body-bg-color': '#22212C',
         'brand-color': '#837E9F',
+        'brand-color-100': '#837E9F11',
         'bg-cards': '#302F3D',
+        'tech-js': '#E7DE79',
+        'tech-vue': '#5FDD5C',
       },
     },
   },
